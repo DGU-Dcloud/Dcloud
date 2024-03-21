3944857c76a9fdc29d7dddeca431abaf730f7385
parkmingyun99 <griffinland9@naver.com>
2024-03-22 03:51:20 +0900

    release:projectUpload

---
+++ src/main/java/dgu/ailab/dcloud/controller/TestController.java
@@ -0,0 +1,13 @@
+package dgu.ailab.dcloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String hello() {
+        return "테스트입니다.";
+    }
+}
---
+++ src/test/java/dgu/ailab/dcloud/DcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package dgu.ailab.dcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
