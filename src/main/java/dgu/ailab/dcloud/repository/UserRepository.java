883ecc04b256b329cff286de870c859c9052ea77
LeeJiYeon <dlwldus2427@dgu.ac.kr>
2024-04-09 20:30:41 +0900

    feat: login api

--- src/main/java/dgu/ailab/dcloud/repository/UserRepository.java
+++ src/main/java/dgu/ailab/dcloud/repository/UserRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByUserID(String userId);
+
+    User findByUserID(String userID);
 }
