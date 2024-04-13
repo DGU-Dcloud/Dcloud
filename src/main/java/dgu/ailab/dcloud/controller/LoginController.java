735ba740a2a1e008fffaabb3d1f21fe622fd4798
parkmingyun99 <griffinland9@naver.com>
2024-04-14 03:26:51 +0900

    feat:session

--- src/main/java/dgu/ailab/dcloud/controller/LoginController.java
+++ src/main/java/dgu/ailab/dcloud/controller/LoginController.java
@@ -2,6 +2,8 @@ package dgu.ailab.dcloud.controller;
 
 import dgu.ailab.dcloud.dto.LoginDto;
 import dgu.ailab.dcloud.service.UserService;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,9 +11,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import jakarta.servlet.http.HttpSession;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")  // CORS 설정 추가
 public class LoginController {
     private final UserService userService;
     private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
@@ -21,16 +24,15 @@ public class LoginController {
     }
 
     @PostMapping("/api/login")
-    public String login(@RequestBody LoginDto loginDto){
+    public String login(@RequestBody LoginDto loginDto, HttpServletRequest request) {
         if (userService.authenticate(loginDto.getUserID(), loginDto.getPassword())) {
-            logger.info("Retrieved login id: {}", loginDto.getUserID());
-            logger.info("Retrieved login pw: {}", loginDto.getPassword());
+            HttpSession session = request.getSession();
+            session.setAttribute(loginDto.getUserID(),loginDto.getUserID());
+            logger.info("Login successful. UserID stored in session: {}", session.getAttribute(loginDto.getUserID()));
             return "Login Successful";
         } else {
-            logger.info("Retrieved login id: {}", loginDto.getUserID());
-            logger.info("Retrieved login pw: {}", loginDto.getPassword());
+            logger.info("Login failed for userID: {}", loginDto.getUserID());
             return "Login Failed";
         }
     }
-
 }
