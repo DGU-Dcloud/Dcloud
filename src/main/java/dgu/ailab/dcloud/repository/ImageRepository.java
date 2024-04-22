9c78455c6d94170c781d48eb288393a263e5fedb
parkmingyun99 <griffinland9@naver.com>
2024-04-22 20:21:40 +0900

    mypage

--- src/main/java/dgu/ailab/dcloud/repository/ImageRepository.java
+++
@@ -1,8 +0,0 @@
-package dgu.ailab.dcloud.repository;
-
-import dgu.ailab.dcloud.entity.DockerImages;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<DockerImages, Long> {
-
-}
